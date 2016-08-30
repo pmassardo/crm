bc84a00cf5e99c219f8fc9926c66f7c59d1c111f
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-08-30 16:59:05 -0400

    Added account edit functionality to the controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,19 +1,56 @@
 class AccountsController < ApplicationController
   def index
+    @accounts = Account.where(user: current_user)
   end
 
   def new
+    @account = Account.new
   end
 
   def create
+
+    @account = Account.new(account_params())
+
+    @account.user = current_user
+
+    if @account.save
+      redirect_to accounts_path
+    else
+      render  :new
+    end
+
   end
 
   def edit
+      @account = Account.find(params[:id])
   end
 
   def update
+
+    # get the account
+    @account = Account.find(params[:id])
+
+    # update the account
+    if @account.update_attributes(account_params())
+      # if it updates
+      # return it to the users accounts list
+      redirect_to accounts_path
+    else
+      # if it does not update
+      # send them back to the edit screen
+      render  :edit
+    end
+
   end
 
   def destroy
   end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:account_type_id, :user_id, :name, :phone, :notes)
+  end
+
+
 end
