40c9d465cfdcf928909d800925c2ac5e93b39222
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-08-30 11:23:46 -0400

    Add account_type and appointment_type primary model/table

---
+++ db/migrate/20160830025112_create_appointment_types.rb
@@ -0,0 +1,9 @@
+class CreateAppointmentTypes < ActiveRecord::Migration
+  def change
+    create_table :appointment_types do |t|
+      t.string :name
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+AccountType.destroy_all
+AccountType.create(name: 'Lead')
+AccountType.create(name: 'Account')
+
+AppointmentType.destroy_all
+AppointmentType.create(name: 'Phone')
+AppointmentType.create(name: 'In-Person')
