911ed1ca5c63e5ae08bedf4a9417ff94d9df2068
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-08-30 13:21:48 -0400

    added the current routes for user and session creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+
+  root :to => 'user_sessions#new'
+
+
+  resources :user_sessions
+resources :users
+
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
+
+
+  # get 'user_sessions/new'
+  #
+  # get 'user_sessions/create'
+  #
+  # get 'user_sessions/destroy'
+  #
+  # resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
