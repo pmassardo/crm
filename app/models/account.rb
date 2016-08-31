3d81863823973bf19d4edd108e287ef5b8df0536
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-08-31 14:08:15 -0400

    add has_many to the account and contact relationship

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,7 @@ class Account < ActiveRecord::Base
   belongs_to :user
 
   # future relationships
-  # has_many :contacts
+  has_many :contacts
   # has_many :appointments, through: :contacts, foreign_key: 'account_id'
 
 end
