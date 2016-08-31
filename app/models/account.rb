4ed40f06b28b24af2ae5ccdc027026f6cc795588
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-08-31 00:29:28 -0400

    added current and future relationship

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,9 @@
 class Account < ActiveRecord::Base
   belongs_to :account_type
   belongs_to :user
+
+  # future relationships
+  # has_many :contacts
+  # has_many :appointments, through: :contacts, foreign_key: 'account_id'
+
 end
