e9dff807ccc927b5582591a65ea40bcb6251d73d
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-02 07:24:52 -0400

    Contacts - added contact saves and deletes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,20 +1,25 @@
 class AccountsController < ApplicationController
   def index
     @accounts = Account.where(user: current_user)
+  #  @contacts = Account.where(user: current_user)
   end
 
   def new
     @account = Account.new
+    @account.contacts.build
+    # binding.pry
   end
 
   def create
 
     @account = Account.new(account_params())
+    # binding.pry
 
     @account.user = current_user
 
     if @account.save
-      redirect_to accounts_path
+    # @account.contacts.build
+      redirect_to edit_account_path(@account)
     else
       render  :new
     end
@@ -23,6 +28,9 @@ class AccountsController < ApplicationController
 
   def edit
       @account = Account.find(params[:id])
+
+      @account.contacts.build()
+
   end
 
   def update
@@ -34,7 +42,9 @@ class AccountsController < ApplicationController
     if @account.update_attributes(account_params())
       # if it updates
       # return it to the users accounts list
-      redirect_to accounts_path
+      # redirect_to accounts_path
+      # render  :edit
+      redirect_to edit_account_path(@account)
     else
       # if it does not update
       # send them back to the edit screen
@@ -49,8 +59,9 @@ class AccountsController < ApplicationController
   private
 
   def account_params
-    params.require(:account).permit(:account_type_id, :user_id, :name, :phone, :notes)
+    params.require(:account).permit(:account_type_id, :user_id, :name, :phone, :notes, contacts_attributes: [:id, :account_id, :first_name, :last_name, :phone, :notes, :email])
   end
+
 
 
 end
