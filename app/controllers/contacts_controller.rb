578c2b0f7d90b89c3ea10417117a1e324eba1d9d
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-02 07:27:19 -0400

    added contact delete code to the destroy action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,19 +1,18 @@
 class ContactsController < ApplicationController
-  def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
   def destroy
+    @account = Account.find(params[:account_id])
+    @contact = @account.contacts.find(params[:id])
+    if @contact.destroy
+      redirect_to edit_account_path(@account)
+    else
+      redirect_to edit_account_path(@account)
+    end
   end
+
+private
+
+  def contact_params
+    params.require(:contact).permit(:account_id, :first_name, :last_name, :phone, :notes, :active)
+  end
+
 end
