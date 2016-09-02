a8148d400a45c453d071b8a8cbd9a5f04b1ef7c8
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-02 07:32:22 -0400

    Change routes so account would control contacts

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
 
   root :to => 'user_sessions#new'
 
-  resources :contacts  # index new create edit update accounts destroy
+  # resources :contacts  # index new create edit update accounts destroy
   resources :accounts  # index new create edit update accounts destroy
   resources :users # index new create edit update accounts destroy
+
+  resources :accounts do
+    resources :contacts, only: [:destroy]
+  end
+
 
   resources :user_sessions # index new create edit update accounts destroy
   get 'login' => 'user_sessions#new', :as => :login
