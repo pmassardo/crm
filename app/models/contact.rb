26730190897a3ffd97d4f41e4d54e1ac2718ff03
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-02 07:29:43 -0400

    Contact - added validation to the contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,13 @@
 class Contact < ActiveRecord::Base
   belongs_to :account
+
+  # has_many :appointments, autosave: true
+
+  # accepts_nested_attributes_for :appointments, reject_if: :all_blank, allow_destroy: true
+
+  validates :first_name, :last_name, presence: true
+
+  validates :phone, presence: true, format: { with: /\d{3}-\d{3}-\d{4}/}
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :email, uniqueness: true
 end
