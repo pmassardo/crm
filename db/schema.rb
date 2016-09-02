b31280e6b12ccffb382069677e6dd1d3bed08282
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-02 07:35:06 -0400

    Contacts - added email and active

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831175518) do
+ActiveRecord::Schema.define(version: 20160901034131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20160831175518) do
     t.text     "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "email"
+    t.boolean  "active",     default: true
   end
 
   add_index "contacts", ["account_id"], name: "index_contacts_on_account_id", using: :btree
