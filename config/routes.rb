a398cf2a553a96baf8bdb26c8c056745248e2cb1
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-03 14:41:13 -0400

    Appointments - add routes for appointments

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,27 @@
 Rails.application.routes.draw do
 
+
+
   root :to => 'accounts#index'
 
   # resources :contacts  # index new create edit update accounts destroy
   resources :accounts  # index new create edit update accounts destroy
   resources :users # index new create edit update accounts destroy
 
+  get 'appointments/new'
+  post 'appointments/create'
+  get 'appointments/edit'
+  post 'appointments/update'
+
+  resources :appointments do
+    collection do
+      get "/schedule/data", action: :schedule, as: :schedule
+    end
+  end
+
   resources :accounts do
     resources :contacts, only: [:destroy]
   end
-
 
   resources :user_sessions # index new create edit update accounts destroy
   get 'login' => 'user_sessions#new', :as => :login
