c9c6ce9db2f0b0659857369d4a18b4a18e955bd2
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-04 16:58:44 -0400

    Routes - updated to include contacts and appointments

--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,27 @@ Rails.application.routes.draw do
   # resources :contacts  # index new create edit update accounts destroy
   resources :accounts  # index new create edit update accounts destroy
   resources :users # index new create edit update accounts destroy
+  resources :appointments
+  resources :contacts, only: [:destroy, :edit, :update, :new, :create]
 
-  get 'appointments/new'
-  post 'appointments/create'
-  get 'appointments/edit'
-  post 'appointments/update'
+  # get 'appointments/new'
+  # post 'appointments/create'
+  # get '/appointments/:id/edit'
+  # post 'appointments/update'
 
-  resources :appointments do
-    collection do
-      get "/schedule/data", action: :schedule, as: :schedule
-    end
-  end
+  # resources :appointments do
+  #   collection do
+  #     get "/schedule/data", action: :schedule, as: :schedule
+  #   end
+  # end
 
   resources :accounts do
     resources :contacts, only: [:destroy]
   end
+
+  get 'contacts' => 'contacts#index', :as => :contacts_list
+
+  # resources :contacts, only: [:index]
 
   resources :user_sessions # index new create edit update accounts destroy
   get 'login' => 'user_sessions#new', :as => :login
