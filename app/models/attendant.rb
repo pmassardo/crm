aae1adc55d4bf30fcd49005de0631d148621a0d2
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-04 16:50:29 -0400

    Attendant - allow nil to all the delegates

--- app/models/attendant.rb
+++ app/models/attendant.rb
@@ -1,4 +1,6 @@
 class Attendant < ActiveRecord::Base
+
+
   belongs_to :appointment , :class_name => Appointment, :foreign_key => "appointment_id"
   belongs_to :contact, :class_name => Contact, :foreign_key => "contact_id"
 
@@ -6,12 +8,12 @@ class Attendant < ActiveRecord::Base
   # and prefix it with the model name
   # so first_name will be contact_first_name
   # phone will be contact_phone
-  delegate :first_name, :to => :contact, :prefix => true
-  delegate :last_name, :to => :contact, :prefix => true
-  delegate :phone, :to => :contact, :prefix => true
-  delegate :email, :to => :contact, :prefix => true
-  delegate :notes, :to => :contact, :prefix => true
-  delegate :id, :to => :contact, :prefix => true
-  delegate :account_id, :to => :contact, :prefix => true
+  delegate :first_name, :to => :contact, :prefix => true , :allow_nil => true
+  delegate :last_name, :to => :contact, :prefix => true, :allow_nil => true
+  delegate :phone, :to => :contact, :prefix => true, :allow_nil => true
+  delegate :email, :to => :contact, :prefix => true, :allow_nil => true
+  delegate :notes, :to => :contact, :prefix => true, :allow_nil => true
+  delegate :id, :to => :contact, :prefix => true, :allow_nil => true
+  delegate :account_id, :to => :contact, :prefix => true, :allow_nil => true
 
 end
