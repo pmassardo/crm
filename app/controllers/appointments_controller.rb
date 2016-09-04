5ee2d87d657763bce44d66b7f98d1d517c3d37ed
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-04 08:17:19 -0400

    Appointments - add debugging code

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,51 +2,59 @@ class AppointmentsController < ApplicationController
 
   def new
 
-    logger.debug params[:selected_contacts]
-
     @appointment = Appointment.new
     @contacts = Contact.find(params[:selected_contacts].map(&:to_i))
 
-    count = 0
-
-    # @appointment.attendants.build
-
     @contacts.each { |contact|
-
-      #
 
       @attendant = Attendant.new
       @attendant.contact_id = contact.id
       @appointment.attendants.push(@attendant)
-      count = count +1
 
     }
-
-    # binding.pry
-
-    logger.debug @contacts.inspect
 
   end
 
   def edit
 
+    logger.debug 'parms ' + params.inspect
+
+    @appointment = Appointment.find(params[:id])
+
+    logger.debug '@appointment ' + @appointment.inspect
 
     # binding.pry
 
-    @appointment = Appointment.find(params[:format])
+  end
+
+  def update
+
+    # get the account
+    @appointment = Appointment.find(params[:id])
+    # logger.debug params[:id]
+
+    # update the account
+    if @appointment.update_attributes(appointment_params())
+      # if it updates
+      # return it to the users appointment list
+      # render  :edit
+      redirect_to edit_appointment_path(@appointment)
+    else
+      # if it does not update
+      # send them back to the edit screen
+      render  :edit
+    end
 
   end
 
   def create
 
-
     @appointment = Appointment.new(appointment_params())
-    # binding.pry
 
     @appointment.user = current_user
 
     if @appointment.save
-      redirect_to appointments_edit_path(@appointment)
+      redirect_to appointments_edit_path(id: @appointment.id)
     else
       render  :new
     end
@@ -58,6 +66,5 @@ class AppointmentsController < ApplicationController
   def appointment_params
     params.require(:appointment).permit(:appointment_type_id, :user_id, :appointment_date, :start_time, :end_time, :notes, attendants_attributes: [:id, :contact_id, :appointment_id])
   end
-
 
 end
