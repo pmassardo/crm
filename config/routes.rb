36d69da30b9c4777cf66037087953a4454db654c
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-04 19:01:00 -0400

    Routes clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,17 @@
 Rails.application.routes.draw do
 
-
-
   root :to => 'accounts#index'
 
-  # resources :contacts  # index new create edit update accounts destroy
-  resources :accounts  # index new create edit update accounts destroy
+  resources :accounts , only: [:index, :edit, :update, :new, :create]
   resources :users # index new create edit update accounts destroy
-  resources :appointments
+  resources :appointments , only: [:edit, :update, :new, :create]
   resources :contacts, only: [:destroy, :edit, :update, :new, :create]
-
-  # get 'appointments/new'
-  # post 'appointments/create'
-  # get '/appointments/:id/edit'
-  # post 'appointments/update'
-
-  # resources :appointments do
-  #   collection do
-  #     get "/schedule/data", action: :schedule, as: :schedule
-  #   end
-  # end
 
   resources :accounts do
     resources :contacts, only: [:destroy]
   end
 
   get 'contacts' => 'contacts#index', :as => :contacts_list
-
-  # resources :contacts, only: [:index]
 
   resources :user_sessions # index new create edit update accounts destroy
   get 'login' => 'user_sessions#new', :as => :login
